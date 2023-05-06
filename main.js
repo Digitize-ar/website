db9b865c9de6f2569e39c72175e284a77ca67fdd
lautaro2004 <lautyxd7@gmail.com>
2023-05-06 19:38:24 -0300

    Web-finalizada
    Se realizo todo el responsive y adaptacion final de la web, funciona el apartado de contacto

--- main.js
+++ main.js
@@ -39,5 +39,38 @@ function toggleDiv(id) {
     if (window.innerWidth < 750) {
       navbar.style.display = "none";
     }
+  });
+
+  btnmenu.addEventListener("click", animateBars)
+  var line1_bars = document.querySelector(".lines1");
+  var line2_bars = document.querySelector(".lines2");
+  var line3_bars = document.querySelector(".lines3");
+
+  function animateBars (){
+    line1_bars.classList.toggle("active_lines1")
+    line2_bars.classList.toggle("active_lines2")
+    line3_bars.classList.toggle("active_lines3")
+  }
+
+  //JS del form//
+
+  const btn = document.getElementById('button');
+
+  document.getElementById('form')
+  .addEventListener('submit', function(event) {
+  event.preventDefault();
+
+   btn.value = 'Enviando..';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_h6p11et';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Enviado');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
     });
-  
+});
